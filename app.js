08809cd6a03a72e0e1cc452f25e9998ddcdb5240
Alex Costa <alexcostars@gmail.com>
2024-09-28 15:46:47 -0300

    substituindo a lib 'paillier-js' pela 'paillier-bigint', já que a anterior está deprecada e já não representa mais a lib adotada pela comunidade Paillier no javascript

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const paillier = require('paillier-js')
+const paillierBigint = require('paillier-bigint')
 const { encryptWithProof, verifyProof } = require('./paillier-range-proof')
 const util = require('util')
 
-const bits = 128
-const {publicKey, privateKey} = paillier.generateRandomKeys(bits)
+async function main() {
+    const bits = 128
+    const { publicKey, privateKey } = await paillierBigint.generateRandomKeys(bits)
 
-const validRange = [41, 48468, 15, 184]
-const value = 184
-const [cipher, proof] = encryptWithProof(publicKey, value, validRange, bits)
+    const validRange = [41n, 48468n, 15n, 454n, 184n]
+    const value = 15n
+    const r = 76532n
+    const [cipher, proof] = encryptWithProof(publicKey, value, validRange, r, bits)
+    console.log(`cipher: ${cipher}`)
 
-console.log("proof: ", util.inspect(proof, {showHidden: false, depth: null, colors: true}))
+    console.log("proof: ", util.inspect(proof, {showHidden: false, depth: null, colors: true}))
 
-//send cipher and proof to the verifier
+    //send cipher and proof to the verifier
 
-const result = verifyProof(publicKey, cipher, proof, validRange)
-console.log(`is cipher into [${validRange}]? ${result}`)
+    const result = verifyProof(publicKey, cipher, proof, validRange)
+    console.log(`is cipher into [${validRange}]? ${result}`)
+
+}
+
+main()
--- paillier-range-proof.js
+++ paillier-range-proof.js
@@ -1,19 +1,15 @@
 // source: https://github.com/framp/paillier-in-set-zkp/blob/master/index.js
 // based on https://paillier.daylightingsociety.org/Paillier_Zero_Knowledge_Proof.pdf
 
-const paillier = require('paillier-js')
 const bigInt = require('big-integer')
 const crypto = require('crypto')
 
-// rEncrypt :: Paillier.PublicKey -> Message
-// Fork from paillier-js - Create Paillier Encryption of message and return the random Paillier.R with the result
-const rEncrypt = function ({ n, g }, message) {
-  const _n2 = n.pow(2)
-  let r
-  do {
-      r = bigInt.randBetween(2, n)
-  } while (r.leq(1))
-  return [r, g.modPow(bigInt(message), _n2).multiply(r.modPow(n, _n2)).mod(_n2)]
+const rand = (bitLength) => {
+  let bytes = bitLength / 8
+  let buf = Buffer.alloc(bytes)
+  crypto.randomFillSync(buf)
+  buf[0] = buf[0] | 128  // first bit to 1 -> to get the necessary bitLength
+  return bigInt.fromArray([...buf], 256)
 }
 
 const getBase2Logarithm = (number, maxBits) => {
@@ -34,11 +30,11 @@ const getCoprime = (target, maxBits) => {
   while (true) {
     const lowerBound = bigInt(2).pow(bits-1).plus(1)
     const size = bigInt(2).pow(bits).subtract(lowerBound)
-    let possible = lowerBound.plus(bigInt.rand(bits)).or(1)
+    let possible = lowerBound.plus(rand(bits)).or(1)
     const result = bigInt(possible)
     if (possible.gt(bigInt(2).pow(1024))) return result
     while(target > 0) {
-      [possible, target] = [target, possible.mod(target)]
+      [possible, target] = [target, bigInt(possible).mod(target)]
     }
     if (possible.eq(bigInt(1))) return result
   }
@@ -47,20 +43,23 @@ const getCoprime = (target, maxBits) => {
 // encryptWithProof :: Paillier.PublickKey -> Message -> [Message] -> Bits
 // Generate a message encryption and a Zero Knowledge proof that the message 
 // is among a set of valid messages
-const encryptWithProof = (publicKey, message, validMessages, bits=512) => {
+const encryptWithProof = (publicKey, message, validMessages, r, bits=512) => {
   const as = []
   const es = []
   const zs = []
 
-  const [random, cipher] = rEncrypt(publicKey, message)
+  const cipher = publicKey.encrypt(message, r)
+  const _cipherBI = bigInt(cipher)
+  const _n2BI = bigInt(publicKey._n2)
+  const _gBI = bigInt(publicKey.g)
 
   const om = getCoprime(publicKey.n, bits)
   const ap = om.modPow(publicKey.n, publicKey._n2)
 
   let mi = null
   validMessages.forEach((mk, i) => {
-    const gmk = publicKey.g.modPow(bigInt(mk), publicKey._n2)
-    const uk = cipher.times(gmk.modInv(publicKey._n2)).mod(publicKey._n2)
+    const gmk = _gBI.modPow(bigInt(mk), _n2BI)
+    const uk = _cipherBI.times(gmk.modInv(_n2BI)).mod(_n2BI)
     if (message === mk) {
       as.push(ap)
       zs.push(null)
@@ -71,9 +70,9 @@ const encryptWithProof = (publicKey, message, validMessages, bits=512) => {
       zs.push(zk)
       const ek = bigInt.randBetween(2, bigInt(2).pow(bits).subtract(1));
       es.push(ek)
-      const zn = zk.modPow(publicKey.n, publicKey._n2)
-      const ue = uk.modPow(ek, publicKey._n2)
-      const ak = zn.times(ue.modInv(publicKey._n2)).mod(publicKey._n2)
+      const zn = zk.modPow(publicKey.n, _n2BI)
+      const ue = uk.modPow(ek, _n2BI)
+      const ak = zn.times(ue.modInv(_n2BI)).mod(_n2BI)
       as.push(ak)
     }
   })
@@ -82,7 +81,7 @@ const encryptWithProof = (publicKey, message, validMessages, bits=512) => {
 
   const esum = es.filter(Boolean).reduce((acc, ek) => acc.plus(ek).mod(bigInt(2).pow(256)), bigInt(0))
   const ep = bigInt(hash, 16).subtract(esum).mod(bigInt(2).pow(256))
-  const rep = random.modPow(ep, publicKey.n)
+  const rep = bigInt(r).modPow(ep, publicKey.n)
   const zp = om.times(rep).mod(publicKey.n)
   es[mi] = ep
   zs[mi] = zp
@@ -98,8 +97,8 @@ const verifyProof = (publicKey, cipher, [as, es, zs], validMessages) => {
   const hash = crypto.createHash('sha256').update(as.join('')).digest('hex');
 
   const us = validMessages.map(mk => {
-    const gmk = publicKey.g.modPow(mk, publicKey._n2)
-    const uk = cipher.times(gmk.modInv(publicKey._n2)).mod(publicKey._n2)
+    const gmk = bigInt(publicKey.g).modPow(mk, bigInt(publicKey._n2))
+    const uk = bigInt(cipher).times(gmk.modInv(bigInt(publicKey._n2))).mod(bigInt(publicKey._n2))
     return uk
   })
 
